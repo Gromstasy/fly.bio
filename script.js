8f301d579bfb1fa47f62b92f1b9a1fa0cfd7b0b6
Gromstasy <145389295+Gromstasy@users.noreply.github.com>
2026-01-17 20:03:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const enter = document.getElementById("enter");
+const card = document.getElementById("card");
+const audio = document.getElementById("audio");
+const play = document.getElementById("play");
+const nick = document.querySelector(".glitch");
+const text = nick.innerText;
+nick.innerText = "";
+
+let i = 0;
+setInterval(() => {
+  if (i < text.length) {
+    nick.innerText += text[i];
+    i++;
+  }
+}, 100);
+
+let playing = false;
+
+enter.onclick = () => {
+  enter.style.display = "none";
+  card.classList.remove("hidden");
+  audio.play();
+  playing = true;
+  play.textContent = "⏸";
+};
+
+play.onclick = () => {
+  if (playing) {
+    audio.pause();
+    play.textContent = "▶";
+  } else {
+    audio.play();
+    play.textContent = "⏸";
+  }
+  playing = !playing;
+};
